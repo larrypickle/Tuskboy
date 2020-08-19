27df68256401a2d83dfe2630690229bc3a985173
Larry Wu <larrywu@usc.edu>
2020-08-18 21:53:47 -0700

    added unity atoms

---
+++ Assets/01_kinship_actual/scripts/CardInventory/CardObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum CardType
+{
+    Hate,
+    Love
+}
+public abstract class CardObject : ScriptableObject
+{
+    public GameObject inventoryDisplay; //this will be the display for the card in inventory
+    public GameObject sprite;
+}
